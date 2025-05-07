649ff8aeffa0128a87821847b4caccd7fddba361
tataka <inoue.takahiro04@gmail.com>
2025-05-07 11:48:56 +0900

    Products変更

--- frontend/src/pages/Products.jsx
+++ frontend/src/pages/Products.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useCart } from "../context/CartContext"; // ← 追加
 
+import Category from '../components/common/Category';
+
 const Products = () => {
   const location = useLocation();
   const [products, setProducts] = useState([]);
@@ -31,28 +33,31 @@ const Products = () => {
   const availableProducts = products.filter(product => product.stock > 0);
 
   return (
-    <div>
-      <h2>{gender} - {category} の商品一覧</h2>
-      {availableProducts.length === 0 ? (
-        <p>該当する商品がありません。</p>
-      ) : (
-        <ul className='ulProducts'>
-          {/* {products.map((product) => ( */}
-          {availableProducts.map((product) => (
-            <li key={product.id}> {/* またはユニークになるように組み合わせる */}
-              <img src={product.image_url} alt={product.name} width="150" />
-              <h3>{product.name}</h3>
-              <p>価格: ¥{product.price}</p>
-              <p>カラー: {product.color}</p>
-              <p>サイズ: {product.size}</p>
-              <button onClick={() => addToCart({ ...product, quantity: 1 })}>
-                カートに追加
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <Category />
+      <main>
+        <h2>{gender} - {category} の商品一覧</h2>
+        {availableProducts.length === 0 ? (
+          <p>該当する商品がありません。</p>
+        ) : (
+          <ul className='ulProducts'>
+            {/* {products.map((product) => ( */}
+            {availableProducts.map((product) => (
+              <li key={product.id}> {/* またはユニークになるように組み合わせる */}
+                <img src={product.image_url} alt={product.name} width="150" />
+                <h3>{product.name}</h3>
+                <p>価格: ¥{product.price}</p>
+                <p>カラー: {product.color}</p>
+                <p>サイズ: {product.size}</p>
+                <button onClick={() => addToCart({ ...product, quantity: 1 })}>
+                  カートに追加
+                </button>
+              </li>
+            ))}
+          </ul>
+        )}
+      </main>
+    </>
   );
 };
 
