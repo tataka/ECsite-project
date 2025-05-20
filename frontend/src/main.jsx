4b4f769ec0002545e7c098e4953eaa9426c0ed8a
tataka <inoue.takahiro04@gmail.com>
2025-05-21 06:36:59 +0900

    一部修正

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { CartProvider } from './context/CartContext'; // CartProvider をインポート
 import { BrowserRouter } from 'react-router-dom';
-// import './assets/styles/App.css'
+import './assets/styles/App.css'
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
